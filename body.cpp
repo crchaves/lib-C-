b248f57b9ce6ea69437cbf10404492df89ae8c2b
moises <moises@moises-VirtualBox.(none)>
2015-12-07 22:02:20 +0100

    New 5th commit

---
+++ body.cpp
@@ -0,0 +1,31 @@
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+
+void function1 (int * x, int * y,int * z);
+void function2 (int * x, int * y,int * z);
+void function3 (int * x, int * y,int * z);
+
+void main() {
+
+
+
+}
+
+void function1 (int * x, int * y,int * z){
+}
+
+void function2 (int * x, int * y,int * z){
+}
+
+void function3 (int * x, int * y,int * z){
+}
