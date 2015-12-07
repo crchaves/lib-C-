b6b19b74dbbeeabdc74c3989f9a5b054086d95a7
moises <moises@moises-VirtualBox.(none)>
2015-12-07 22:32:10 +0100

    Header with functions commit

---
+++ header.cpp
@@ -0,0 +1,16 @@
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+#include <stdin.h>
+
+
+#Protocol algorithm header booting
+
+void head ();
+void data ();
+void crc ();
+void check ();
